46eddb2623ed82d132d74e1982b1bb90a7c59b38
loyelnaz <nazloyel59@gmail.com>
2017-10-19 10:52:08 +0530

    instructor login controller

--- src/controller/instructor/LoginInstructor.java
+++ src/controller/instructor/LoginInstructor.java
@@ -1,10 +1,15 @@
 package controller.instructor;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import database.DBLogin;
 
 /**
  * Servlet implementation class LoginInstructor
@@ -32,8 +37,35 @@ public class LoginInstructor extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+
+		RequestDispatcher rd = null;
+		HttpSession session = request.getSession(true);
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		DBLogin dblogin = new DBLogin();
+		int result = dblogin.isLogged(username, password, "user_table", "instructor");
+		
+		if(result > 0) {
+			session.setAttribute("user_id", result);
+			rd = request.getRequestDispatcher("/views/instructor/homepage");
+			rd.forward(request, response);
+		}
+		else if(result == 0) {
+			request.setAttribute("error", "The password entered is incorrect. Please try again.");
+			rd = request.getRequestDispatcher("login.jsp");
+			rd.include(request, response);
+		}
+		else if(result == -1) {
+			request.setAttribute("error", "User does not exists! Register for an account.");
+			rd = request.getRequestDispatcher("login.jsp");
+			rd.include(request, response);
+		}
+		else {
+			request.setAttribute("error", "User is inactive. Contact administrator for the approval");
+			rd = request.getRequestDispatcher("login.jsp");
+			rd.include(request, response);
+		}
 	}
 
 }
