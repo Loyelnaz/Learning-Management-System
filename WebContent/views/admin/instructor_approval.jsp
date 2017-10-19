5458c296650def802565ddc5cf9d55c77e7bbc89
loyelnaz <nazloyel59@gmail.com>
2017-10-19 13:37:13 +0530

    instructor approval view and controller

--- WebContent/views/admin/instructor_approval.jsp
+++ WebContent/views/admin/instructor_approval.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+    <%@page import="java.sql.*" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -7,7 +8,7 @@
 <title>Insert title here</title>
 </head>
 <body>
-
-<a href="instructor_approval.jsp">Approve Instructor</a>
+	Hi Instructor Approval page!
+	
 </body>
 </html>
---
+++ src/controller/admin/InstructorApproval.java
@@ -0,0 +1,63 @@
+package controller.admin;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.*;
+import database.*;
+
+/**
+ * Servlet implementation class InstructorApproval
+ */
+public class InstructorApproval extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public InstructorApproval() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		Connection conn = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		
+		conn = new DBConnector().getConnection();
+		try {
+			ps = conn.prepareStatement("select * from user_table where is_active=?"
+										+ "and ugid=(select ugid from user_group where group_name=?)");
+			ps.setBoolean(1, false);
+			ps.setString(2, "instructor");
+			rs = ps.executeQuery();
+			while(rs.next()) {
+				System.out.println("Name: " + rs.getString(2));
+			}
+			request.setAttribute("inactive", rs);
+			request.getRequestDispatcher("instructor_approval.jsp").forward(request, response);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
