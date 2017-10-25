0253ef2286ea8819979ea24c61d8a059481ce702
loyelnaz <nazloyel59@gmail.com>
2017-10-25 16:29:49 +0530

    quiz score calculation

--- src/controller/student/QuizResultController.java
+++ src/controller/student/QuizResultController.java
@@ -54,6 +54,7 @@ public class QuizResultController extends HttpServlet {
 //		System.out.println(option11);
 		
 		try {
+			int quiz_score = 0;
 			ps = conn.prepareStatement("select * from quiz_questions where course_id=? and module_no=?");
 			ps.setInt(1, course_id);
 			ps.setInt(2, module_no);
@@ -64,10 +65,29 @@ public class QuizResultController extends HttpServlet {
 				String option2 = request.getParameter("option2"+quiz.getInt(9));
 				String option3 = request.getParameter("option3"+quiz.getInt(9));
 				String option4 = request.getParameter("option4"+quiz.getInt(9));
-				System.out.println(option1);
-				System.out.println(option2);
-				System.out.println(option3);
+
+				if(option1 != null) {
+					if(option1.equals(quiz.getString(8))) {
+						quiz_score = quiz_score + 1;
+					}
+				}
+				else if(option2 != null) {
+					if(option2.equals(quiz.getString(8))) {
+						quiz_score = quiz_score + 1;
+					}
+				}
+				else if(option3 != null) {
+					if(option3.equals(quiz.getString(8))) {
+						quiz_score = quiz_score + 1;
+					}
+				}
+				else if(option4 != null) {
+					if(option4.equals(quiz.getString(8))) {
+						quiz_score = quiz_score + 1;
+					}
+				}
 			}
+			System.out.println("Quiz Score: " + quiz_score);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
